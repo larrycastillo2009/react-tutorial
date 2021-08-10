56eaac591fc162f7e2bddd2217a8f24390978e01
Larry Castillo <larrycastillo2009@gmail.com>
2021-08-10 15:06:49 -0500

    Table component created

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Table from './Table'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App1 extends React.Component{
+    render(){
+        return(
+            <div className={App1}>
+                <h1>Hello, React!</h1>
+                <Table/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App1 />, document.getElementById('root'))
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
