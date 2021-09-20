3d1af710ea5c99644e0f837034986d387931debe
Larry Castillo <larrycastillo2009@gmail.com>
2021-09-20 10:45:23 -0500

    app refractor

--- src/index.js
+++ src/index.js
@@ -7,38 +7,9 @@ import Form from './Form'
 import * as serviceWorker from './serviceWorker';
 
 
-class App1 extends React.Component {
-    state = {
-        characters: [],
-    }
 
-    removeCharacter = (index) => {
-        const {characters} = this.state
 
-        this.setState({
-            characters: characters.filter((character, i) => {
-                return i !== index
-            }),
-        })
-    }
-
-    handleSubmit = (character) => {
-        this.setState({characters: [...this.state.characters, character]})
-    }
-
-    render() {
-        const{ characters } = this.state
-        return (
-            <div className="container">
-                <Table characterData={characters}
-                       removeCharacter={this.removeCharacter}/>
-                <Form handleSubmit={this.handleSubmit} />
-            </div>
-        )
-    }
-}
-
-ReactDOM.render(<App1/>, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
 
 // ReactDOM.render(
 //   <React.StrictMode>
